e4bf78539470d776557020eb4907abee50eeac78
Alishba-Bacha <130219545+Alishba-Bacha@users.noreply.github.com>
2023-09-29 23:50:04 +0500

    28

---
+++ hidden.py
@@ -0,0 +1,8 @@
+#https://apps.twitter.com/
+#Create new App and get four strings
+
+def oauth():
+    return{"consumer_Key": "h7lu...Ng",
+           "consumer_secret":"dNKenAC3New...mmn7Q",
+           "token_key": "10185562-eibxCp92...p4GEQQOSGI",
+           "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
